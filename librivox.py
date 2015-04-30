7062f0c1dee1699f650a59af50ab49e3320394ca
Eric Rochester <erochest@virginia.edu>
2015-04-30 15:20:16 -0400

    Added a resumable work queue.
    * spools both the work queue and the output into a database
      (transactions FTW!)
    * more status messages
    * fixed some bugs

--- librivox.py
+++ librivox.py
@@ -1,5 +1,5 @@
+from contextlib import closing
 from urllib import request
-from urllib import parse as urlparse
 from bs4 import BeautifulSoup
 import re
 from nltk import word_tokenize
@@ -7,20 +7,27 @@ from nltk.corpus import words
 import math
 import time
 import random
+import sqlite3
+import os
 
 posts =[]
 counter = 0
 max_sleep = 2.0
 post_counter = 1
 
+FORUM = 'f'
+TOPIC = 't'
+
+
 def download(url):
     time.sleep(random.random() * max_sleep)
-    html = request.urlopen(url).read().decode('utf8')
+    print('DOWNLOAD {}'.format(url))
+    html = request.urlopen(url).read().decode('utf8', errors='replace')
     return BeautifulSoup(html)
 
 
-def get_urls(soup, class_, key):
-    """ Returns a tuple (URL, ID, text). """
+def get_urls(soup, class_, key, parent_id):
+    """ Returns a tuple (key, URL, ID, text). """
     begin_pattern = re.compile(r'^\.')
     end_pattern = re.compile(r'&sid=.+$')
     urls = []
@@ -30,13 +37,45 @@ def get_urls(soup, class_, key):
             continue
         url = end_pattern.sub('', url)
         url = begin_pattern.sub('https://forum.librivox.org', url)
-        # Parse the URL to get the page's identifier.
-        urlp = urlparse.urlparse(url)
-        qs = urlparse.parse_qs(urlp.query)
-        ids = [int(url_id) for url_id in qs.get(key, ['-1'])]
-
-        urls.append((url, ids[0], a.get_text()))
+        urls.append((key, url, parent_id, a.get_text()))
     return urls
+
+
+def get_work(cxn):
+    """\
+    Retrieve one unscraped URL from the database and update its scraped field.
+
+    Each work item is a three-part tuple listing the item's ID, URL, and level.
+    """
+    with closing(cxn.cursor()) as c:
+        c.execute('''
+            SELECT id, url, level
+                FROM urls
+                WHERE scraped IS NULL
+                LIMIT 1;
+            ''')
+        work = c.fetchone()
+        if work is not None:
+            c.execute("UPDATE urls SET scraped=datetime('now') WHERE id=?;",
+                      (work[0],))
+        return work
+
+
+def enqueue_urls(cxn, urls):
+    """This takes the output of get_urls and inserts them into the db."""
+    with closing(cxn.cursor()) as c:
+        print('enqueue_urls: {}'.format(urls))
+        c.executemany(
+            '''INSERT INTO urls
+                (level, url, parent_url_id, text)
+                VALUES (?, ?, ?, ?);''',
+            urls,
+            )
+
+
+def get_enqueue_urls(cxn, soup, class_, key, parent_id):
+    """Get the urls via 'get_urls' and puts them into the database."""
+    enqueue_urls(cxn, get_urls(soup, class_, FORUM, parent_id))
 
 
 def scrape_posts(url):
@@ -95,9 +134,16 @@ def find_number_of_pages_or_topics(url):
 
         return number_of_pages
 
-def scrape_topic(forum_id, forum_name, topic_url, topic_id, topic_name):
+
+def insert_posts(cxn, posts):
+    """Take the output of scrape_topic and insert it into the posts."""
+    with closing(cxn.cursor()) as c:
+        c.executemany('INSERT INTO postings (url_id, text) AS (?, ?);', posts)
+
+
+def scrape_topic(topic_url, topic_id):
         """scrapes all posts from a topic"""
-        output = open('output.txt', 'a')
+        output = []
         counter = 0
         global post_counter
 
@@ -114,54 +160,99 @@ def scrape_topic(forum_id, forum_name, topic_url, topic_id, topic_name):
                 url = paginator(url, counter, 15)
                 posts = scrape_posts(url)
                 for post in posts:
-                        output.write('\t'.join([
-                            str(forum_id), forum_name,
-                            str(topic_id), topic_name,
-                            str(post.replace('\t', ' ')),
-                            ]))
-                        output.write('\n\n\n')
+                        output.append(
+                            (str(topic_id), str(post).replace('\t', ' ')),
+                            )
                         print(url + '    ' + str(post_counter))
                         post_counter += 1
                 counter += 1
 
-        output.close
+        return output
 
-def get_all_topic_links_in_a_forum(forum_url):
+def get_all_topic_links_in_a_forum(forum_url, forum_id):
         """gets all the topic links in a forum."""
         links = []
         counter = 0
         num_pages = find_number_of_pages_or_topics(forum_url)
 
-        url = forum_url + '&t='
+        url = forum_url + '&start=0'
 
         while counter < num_pages:
                 url = paginator(url, counter, 50)
-                page_links = get_topic_links_for_a_page(forum_url)
+                page_links = get_topic_links_for_a_page(url, forum_id)
                 links.extend(page_links)
                 counter += 1
 
         return(links)
 
 
-def get_topic_links_for_a_page(forum_url):
+def get_topic_links_for_a_page(forum_url, forum_id):
         """Gets all of the links for the topics contained in a forum."""
         # note: it only does this for one page. You need to paginate
         # pulls in all the raw links with the topic title class.
-        return get_urls(download(forum_url), 'topictitle', 't')
+        return get_urls(download(forum_url), 'topictitle', TOPIC, forum_id)
 
-def scrape_forum(forum_url, forum_id, forum_name):
+def scrape_forum(forum_url, forum_id):
         """Scrapes all the topics for a given forum."""
 
         # for every topic in a forum, scrape all the posts and put it in a single list.
-        for link_info in get_all_topic_links_in_a_forum(forum_url):
-                scrape_topic(forum_id, forum_name, *link_info)
+        return get_all_topic_links_in_a_forum(forum_url, forum_id)
 
 def scrape_everything():
         """scrapes everything. punch it chewie."""
         # pulls in the main index page
-        soup = download('https://forum.librivox.org/index.php')
-        for forum_info in get_urls(soup, 'forumlink', 'f'):
-                scrape_forum(*forum_info)
+        with open_db('librivox.db') as cxn:
+            soup = download('https://forum.librivox.org/index.php')
+            get_enqueue_urls(cxn, soup, 'forumlink', FORUM, None)
+            cxn.commit()
+
+            while True:
+                try:
+                    work = get_work(cxn)
+                    if work is None:
+                        break
+                    print('WORK: {}'.format(work))
+
+                    if work[2] == FORUM:
+                        enqueue_urls(cxn, scrape_forum(work[1], work[0]))
+                    elif work[2] == TOPIC:
+                        insert_posts(cxn, scrape_topic(work[1], work[0]))
+
+                except:
+                    cxn.rollback()
+                    raise
+
+                else:
+                    cxn.commit()
+
+
+def open_db(filename):
+    exists = os.path.exists(filename)
+    cxn = sqlite3.connect(filename)
+
+    if not exists:
+        with closing(cxn.cursor()) as c:
+            c.executescript('''
+                CREATE TABLE urls (
+                    id INTEGER PRIMARY KEY,
+                    url TEXT,
+                    parent_url_id INTEGER,
+                    level TEXT,
+                    text TEXT,
+                    added REAL DEFAULT (datetime('now')),
+                    scraped REAL DEFAULT NULL
+                    );
+                CREATE TABLE postings (
+                    id INTEGER PRIMARY KEY,
+                    url_id INTEGER,
+                    text TEXT,
+                    scraped REAL DEFAULT (datetime('now'))
+                    );
+                ''')
+            cxn.commit()
+
+    return cxn
+
 
 if __name__ == '__main__':
     scrape_everything()
