9558049d0885f1586318bfc53ca2db47c9334290
Brandon Walsh <bmw9t@virginia.edu>
2015-05-08 16:44:35 -0400

    33.py

---
+++ ch_three/33.py
@@ -0,0 +1,13 @@
+# ◑ The index() function can be used to look up items in sequences. For example, 'inexpressible'.index('e') tells us the index of the first position of the letter e.
+
+# What happens when you look up a substring, e.g. 'inexpressible'.index('re')?
+
+# Define a variable words containing a list of words. Now use words.index() to look up the position of an individual word.
+
+# Define a variable silly as in the exercise above. Use the index() function in combination with list slicing to build a list phrase consisting of all the words up to (but not including) in in silly.
+
+words = 'function in combination with list slicing to build a list phrase consisting of all the words up to (but not including) in in silly.'
+
+words = words.split(' ')
+
+silly = 'newly formed bland ideas are inexpressible in an infuriating way'
