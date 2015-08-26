0c336cdd47118912db8ebb2d36f3c128c7db3934
Brandon Walsh <bmw9t@virginia.edu>
2015-08-26 09:24:22 -0400

    fixes to 32

--- ch_five/32.py
+++ ch_five/32.py
@@ -1,5 +1,8 @@
 # ◑ Consult the documentation for the Brill tagger demo function, using help(nltk.tag.brill.demo). Experiment with the tagger by setting different values for the parameters. Is there any trade-off between training time (corpus size) and performance?
 
+import nltk
+from nltk.corpus import brown
+
 from nltk.tag.brill import *
 from nltk.tag import brill_trainer
 
