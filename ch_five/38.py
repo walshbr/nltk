a790982cc9893f84b92bd4b52667c09efdc47242
Brandon Walsh <bmw9t@virginia.edu>
2015-09-01 11:10:26 -0400

    new questions

---
+++ ch_five/38.py
@@ -0,0 +1,3 @@
+# ★ Consider the code in 5 which determines the upper bound for accuracy of a trigram tagger. Review Abney's discussion concerning the impossibility of exact tagging (Church, Young, & Bloothooft, 1996). Explain why correct tagging of these examples requires access to other kinds of information than just words and tags. How might you estimate the scale of this problem?
+
+Correct tagging requires you to basically have the entire context of the english language, right? So you need much more information than a computer can ever have. The scale of this problem is such that as you approach an infinite training size you approach maximum accuracy.
