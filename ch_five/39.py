f916fc0626536e863e1bb8ef501f67901b3fd1cc
Brandon Walsh <bmw9t@virginia.edu>
2015-09-01 15:08:38 -0400

    questions

---
+++ ch_five/39.py
@@ -0,0 +1,2 @@
+# ★ Use some of the estimation techniques in nltk.probability, such as Lidstone or Laplace estimation, to develop a statistical tagger that does a better job than n-gram backoff taggers in cases where contexts encountered during testing were not seen during training.
+
